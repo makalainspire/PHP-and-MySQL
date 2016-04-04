4f086ed81085d0a4aae4acb11679997b321b1bea
Charilla <makalainspiration@gmail.com>
2016-04-04 14:59:58 -0600

    Create InterestArray.php

---
+++ Chapter-1/InterestArray.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML public "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Arithmetic Examples</title>
+<meta http-equiv="content-type" content="text/html"; charset="iso-8859-1" />
+</head>
+<body>
+    <?php
+        $RatesArray = array(.0725, .0750, .0775, .0800, .0825, .0850, .0875);
+        echo "<p>$RatesArray[0]<br />";
+        echo "$RatesArray[1]<br />";
+        echo "$RatesArray[2]<br />";
+        echo "$RatesArray[3]<br />";
+        echo "$RatesArray[4]<br />";
+        echo "$RatesArray[5]<br />";
+        echo "$RatesArray[6]<br />";
+        echo "$RatesArray[7]</p>";
+    ?>
+</body>
+</html>
