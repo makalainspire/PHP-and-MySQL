f3573e629b6a11c2e5bf96fe2a0be2c550a6c6d8
Charilla <makalainspiration@gmail.com>
2016-04-04 14:59:11 -0600

    Create SingleFamilyHome.php

---
+++ Chapter-1/SingleFamilyHome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML public "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Days Array</title>
+<meta http-equiv="content-type" content="text/html"; charset="iso-8859-1" />
+</head>
+<body>
+    <?php
+      $SingleFamilyHome = 399500;
+      $SingleFamilyHome_Display = number_format($SingleFamilyHome, 2);
+      echo "<p>The current median price of a single family home in Pleasanton, CA is $$SingleFamilyHome_Display.</p>";
+    ?>
+</body>
+</html>
