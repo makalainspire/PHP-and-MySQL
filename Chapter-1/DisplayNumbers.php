75b719ff19ee62c96d95e3e5938934aeba1bc3be
Charilla <makalainspiration@gmail.com>
2016-04-04 14:57:54 -0600

    Create DisplayNumbers.php

---
+++ Chapter-1/DisplayNumbers.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML public "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Display Numbers</title>
+<meta http-equiv="content-type" content="text/html"; charset="iso-8859-1" />
+</head>
+<body>
+    <?php
+      $IntegerVar = 150;
+      $FloatingPointVar = 3.0e7;
+      echo "<p>Integer Variable: $IntegerVar<br />";
+      echo "Floating-point variable: $FloatingPointVar</p>";
+    ?>
+</body>
+</html>
